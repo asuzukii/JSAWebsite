d621589b1bd6eefd36a0820dc5bcd9abe567c64f
Akira Suzuki <akira.suzuki@cepton.com>
2021-08-20 01:07:32 -0700

    finished form

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import { useDispatch } from 'react-redux';
 
-import Posts from './components/Posts/Posts.js'
-import Form from './components/Form/Form.js'
-import memories from './images/memories.png'
+import { getPosts } from './actions/posts';
+import Posts from './components/Posts/Posts.js';
+import Form from './components/Form/Form.js';
+import memories from './images/memories.png';
+import useStyles from './styles.js';
+
 const App = () => {
+    const classes = useStyles();
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(getPosts());
+    }, [dispatch]);
     return (
         <Container maxWidth="lg">
-            <AppBar position="static" color="inherit">
-                <Typography variant="h2" align="center">Memories</Typography>
-                <img src={memories} alt="memories" height="60" />
+            <AppBar className={classes.appBar} position="static" color="inherit">
+                <Typography className={classes.heading} variant="h2" align="center">Memories</Typography>
+                <img className={classes.image} src={memories} alt="memories" height="60" />
             </AppBar>
             <Grow in>
                 <Container>
